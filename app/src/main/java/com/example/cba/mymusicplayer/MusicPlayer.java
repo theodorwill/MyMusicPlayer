23d8a604987f2d1209195f35ea3d2541ce182218
cba <theodor.will@gmail.com>
2017-04-02 20:08:21 +0200

    custom toggleButton o lite kod

---
+++ app/src/main/java/com/example/cba/mymusicplayer/MusicPlayer.java
@@ -0,0 +1,16 @@
+package com.example.cba.mymusicplayer;
+
+import android.os.Bundle;
+
+/**
+ * Created by cba on 2017-04-02.
+ */
+
+public class MusicPlayer extends MainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
